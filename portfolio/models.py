bf249c305af53db2a5cc4c305f7c714c33138eb3
soumya ranjan <smartsoumya02@gmail.com>
2022-04-17 10:39:06 +0530

    First commit!

---
+++ portfolio/models.py
@@ -0,0 +1,16 @@
+from distutils.command import upload
+from email.mime import image
+from operator import mod
+from pyexpat import model
+from turtle import title
+from django.db import models
+
+class Project(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.CharField(max_length=250)
+    image=models.ImageField(upload_to='portfolio/images/')
+    url=models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
